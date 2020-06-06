c9574187209a2dae30d8277253bac2e487dbe06a
root <root@ip-172-31-40-139.us-east-2.compute.internal>
2020-06-06 16:49:20 +0000

    seconn

--- index.php
+++ index.php
@@ -2,3 +2,22 @@
 echo "mdhack";
 echo /n/n;
 ?>
+
+
+
+
+<html>
+	<head>
+	<title>PHP mayank Test</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1"/>
+	</head>
+
+<body>
+	<h1>PHP 1662Test</h1>
+		<p><b>An Example of PHP in Action</b></p>
+		<?php echo "The Current Date and Time is: <br />";
+		echo date("g:i A l, F j Y.");?> </p>
+	<h2>PHP Information VERSION2 </h2>
+		<p> <?php phpinfo(); ?> </p>
+	</body>
+</html>
