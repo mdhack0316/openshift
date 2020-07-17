4c3357857048030ea5e9c783819e173f54948e96
root <root@ip-172-31-40-139.us-east-2.compute.internal>
2020-07-17 06:25:47 +0000

    Hi

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ echo /n/n;
 		<p><b>An Example of PHP in Action</b></p>
 		<?php echo "The Current Date and Time is: <br />";
 		echo date("g:i A l, F j Y.");?> </p>
-	<h2>PHP Information VERSION2 </h2>
+	<h2>PHP Information VERSION10 </h2>
 		<p> <?php phpinfo(); ?> </p>
 	</body>
 </html>
