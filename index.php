f1e9f47e87c5eeb3d5d582a787bf6cc19bf985c8
root <root@ip-172-31-37-247.us-east-2.compute.internal>
2020-08-21 10:29:01 +0000

    This is version 2

--- index.php
+++ index.php
@@ -8,12 +8,12 @@ echo /n/n;
 
 <html>
 	<head>
-	<title>PHP mayanewchnages i am doingnk Test</title>
+	<title>Training is on DO285</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1"/>
 	</head>
 
 <body>
-	<h1>PHP 186865kContetn chnage tc76Test</h1>
+	<h1>Training is ON DO285t</h1>
 		<p><b>An Example of PHP in Action</b></p>
 		<?php echo "The Current Date and Time is: <br />";
 		echo date("g:i A l, F j Y.");?> </p>
